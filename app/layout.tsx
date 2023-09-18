90e9dbb98ca292968329d2575304ffdf3b38c69d
Wannyon <ghkdwodnjs123@naver.com>
2023-09-18 23:52:32 +0900

    2023-09-18 <레이아웃 작업 시작> Layout -> 헤더와 푸터 작업 TODO -> 헤더&푸터 수치 조정 필요, 메인 영역 flex-grow 적용안됨

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,55 @@
+import styles from './layout.module.css';
+import './globals.css';
+import Link from 'next/link'
+import FooterComponent from '@/src/serverComponent/FooterComponent';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>pet-k-in</title>
+  </head>
+      <body>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <nav className={styles.nav}>
+              <div className={styles.logo}>
+                {/* 로고 이미지 */}
+                Logo
+              </div>
+              <ul className={styles.navList}>
+                <li><Link href="/map">명동여지도</Link></li>
+                <li><Link href="/animalinfo">반려동물 정보</Link></li>
+                <li><Link href="/event">이벤트</Link></li>
+                <li><Link href="/community">커뮤니티</Link></li>
+                <li><Link href="/service">고객센터</Link></li>
+                {/* 다른 페이지 링크를 추가하세요. */}
+              </ul>
+            </nav>
+
+            <div className={styles.headerRight}>
+              로그인
+              {/* 비로그인 상태 : 가입/로그인 */}
+              {/* 로그인 상태 : 프로필/알림 */}
+            </div>
+            
+          </header>
+          <main className={styles.main}>
+            {children}
+            {/* 페이지의 내용은 여기에 렌더링됩니다. */}
+          </main>
+
+          <FooterComponent/>
+        </div>
+      </body>
     </html>
   );
-}
+};
+
---
+++ src/serverComponent/FooterComponent.tsx
@@ -0,0 +1,13 @@
+import styles from '../../app/layout.module.css';
+
+function FooterComponent () {
+
+    return (
+      <footer className={styles.footer}>
+            {/* 푸터 내용 */}
+            <p>&copy; {new Date().getFullYear()} Pet-K-In</p>
+      </footer>
+    );
+  };
+
+export default FooterComponent;
