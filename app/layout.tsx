d5d5765346e97174baee8f9e98ecb2e0de3db9e3
Wannyon <ghkdwodnjs123@naver.com>
2023-09-20 00:03:59 +0900

    prettier 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import styles from './layout.module.css';
 import './globals.css';
-import Link from 'next/link'
+import Link from 'next/link';
 import FooterComponent from '@/src/serverComponent/FooterComponent';
 
 export default function RootLayout({
@@ -8,14 +8,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <head>
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>pet-k-in</title>
-  </head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>pet-k-in</title>
+      </head>
       <body>
         <div className={styles.container}>
           <header className={styles.header}>
@@ -25,11 +24,21 @@ export default function RootLayout({
                 Logo
               </div>
               <ul className={styles.navList}>
-                <li><Link href="/map">명동여지도</Link></li>
-                <li><Link href="/animalinfo">반려동물 정보</Link></li>
-                <li><Link href="/event">이벤트</Link></li>
-                <li><Link href="/community">커뮤니티</Link></li>
-                <li><Link href="/service">고객센터</Link></li>
+                <li>
+                  <Link href="/map">명동여지도</Link>
+                </li>
+                <li>
+                  <Link href="/animalinfo">반려동물 정보</Link>
+                </li>
+                <li>
+                  <Link href="/event">이벤트</Link>
+                </li>
+                <li>
+                  <Link href="/community">커뮤니티</Link>
+                </li>
+                <li>
+                  <Link href="/service">고객센터</Link>
+                </li>
                 {/* 다른 페이지 링크를 추가하세요. */}
               </ul>
             </nav>
@@ -39,17 +48,15 @@ export default function RootLayout({
               {/* 비로그인 상태 : 가입/로그인 */}
               {/* 로그인 상태 : 프로필/알림 */}
             </div>
-            
           </header>
           <main className={styles.main}>
             {children}
             {/* 페이지의 내용은 여기에 렌더링됩니다. */}
           </main>
 
-          <FooterComponent/>
+          <FooterComponent />
         </div>
       </body>
     </html>
   );
-};
-
+}
--- src/serverComponent/FooterComponent.tsx
+++ src/serverComponent/FooterComponent.tsx
@@ -1,13 +1,12 @@
 import styles from '../../app/layout.module.css';
 
-function FooterComponent () {
-
-    return (
-      <footer className={styles.footer}>
-            {/* 푸터 내용 */}
-            <p>&copy; {new Date().getFullYear()} Pet-K-In</p>
-      </footer>
-    );
-  };
+function FooterComponent() {
+  return (
+    <footer className={styles.footer}>
+      {/* 푸터 내용 */}
+      <p>&copy; {new Date().getFullYear()} Pet-K-In</p>
+    </footer>
+  );
+}
 
 export default FooterComponent;
